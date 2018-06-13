ec6a230190a78613dcaed7fc20a58c5a9921e942
Dion Niazi <dion1008@Dions-MBP.home>
2018-06-12 21:46:37 -0400

    Fixing these files

---
+++ lab05/inlab/binarynode.h
@@ -0,0 +1,19 @@
+// Dion Niazi dn3gy 01 03 2017 binarynode.h
+/* binarynode.h: binary node class definition */
+#ifndef BINARY_NODE_H
+#define BINARY_NODE_H
+
+#include <string>
+using namespace std;
+
+class BinaryNode {
+    string element;
+    BinaryNode *left;
+    BinaryNode *right;
+
+    BinaryNode( const string & theElement, BinaryNode *lt, BinaryNode *rt )
+        : element( theElement ), left( lt ), right( rt ) { }
+    friend class BinarySearchTree;
+};
+
+#endif
